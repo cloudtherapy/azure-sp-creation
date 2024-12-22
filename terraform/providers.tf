1d15501597627f327ab0e81354959ec72201a24c
clubcaliche <cmoreira@misfirm.com>
2024-12-22 00:18:29 -0500

    Fix variables

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -18,12 +18,12 @@ terraform {
 # Azure Active Directory provider
 # https://registry.terraform.io/providers/hashicorp/azuread/latest/docs
 provider "azuread" {
-  tenant_id = var.tenant_id
+  tenant_id = data.azurerm_subscription.this.tenant_id
 }
 
 # Azure Resource Manager provider
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  subscription_id = data.azurerm_subscription.this.subscription_id
 }
