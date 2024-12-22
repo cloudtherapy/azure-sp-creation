98baca11b63d1492c717ee89e877401c82f52769
clubcaliche <cmoreira@misfirm.com>
2024-12-22 00:26:53 -0500

    Moved providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,15 +15,3 @@ terraform {
   }
 }
 
-# Azure Active Directory provider
-# https://registry.terraform.io/providers/hashicorp/azuread/latest/docs
-provider "azuread" {
-  tenant_id = data.azuread_client_config.this.tenant_id
-}
-
-# Azure Resource Manager provider
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-provider "azurerm" {
-  features {}
-  subscription_id = data.azurerm_subscription.this.id
-}
