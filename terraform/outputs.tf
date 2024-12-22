55e660f61a1703c2838b3aec512a1acc9736cfd4
clubcaliche <cmoreira@misfirm.com>
2024-12-22 01:03:10 -0500

    updated variable

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,7 +11,7 @@ output "client_secret" {
 }
 
 output "subscription_id" {
-  value = data.azurerm_subscription.this.subscription_id.id
+  value = data.azurerm_subscription.this.id
 }
 
 output "tenant_id" {
