3f211d1d846aee982186d137dc4d8d1754349812
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-22 21:45:17 -0500

    Updated end_date

--- cloud_shell/03-app_credential.sh
+++ cloud_shell/03-app_credential.sh
@@ -1,7 +1,8 @@
 . ./environment
 
 # Create appID and secret to be utilized
-az ad app credential reset --id ${object_id} >> ./secret.json
+az ad app credential reset --id ${object_id} \
+--end-date "2299-12-30T23:00:00Z" >> ./secret.json
 
 # Store appID and secret in environment file
 client_id=`cat secret.json | jq -r '.appId'`
