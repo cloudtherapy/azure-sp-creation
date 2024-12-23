a8b835da457a154dc66d05612c3032a0dd74d59e
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-22 21:57:36 -0500

    Tidy up var names

--- cloud_shell/xx_cleanup.sh
+++ cloud_shell/xx_cleanup.sh
@@ -3,20 +3,20 @@
 # Destroy Application including Enterprise App
 az ad app delete --id ${object_id}
 
-# Delete OBJECT_ID from environment file
+# Delete app_name from environment file
+sed -i '/app_name/d' ./environment
+
+# Delete object_ID from environment file
 sed -i '/object_id/d' ./environment
 
-# Delete SUBSCRIPTION_ID from environment file
+# Delete subscription_id from environment file
 sed -i '/subscription_id/d' ./environment
 
-# Delete APP_ID from environment file
+# Delete client_id from environment file
 sed -i '/client_id/d' ./environment
 
-# Delete SECRET_ID from environment file
+# Delete client_secret from environment file
 sed -i '/client_secret/d' ./environment
 
-# Delete SP_OBJECT_ID from environment file
+# Delete sp_object_id from environment file
 sed -i '/sp_object_id/d' ./environment
-
-# Delete SUBSCRIPTION_ID from environment file
-sed -i '/subscription_id/d' ./environment
