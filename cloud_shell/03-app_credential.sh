27e6e3114168f93f930c92d512c53222b037e61b
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-26 22:50:33 -0500

    Added tenant_id to environment

--- cloud_shell/03-app_credential.sh
+++ cloud_shell/03-app_credential.sh
@@ -3,3 +3,6 @@
 # Create appID and secret to be utilized
 az ad app credential reset --id ${object_id} \
 --end-date "2299-12-30T23:00:00Z" >> ./secret.tfvars.json
+
+tenant_id=`cat ./secrets.tfvars.json | jq -r '.tenant_id'`
+echo "tenant_id="${tenant_id} >> ./environment
