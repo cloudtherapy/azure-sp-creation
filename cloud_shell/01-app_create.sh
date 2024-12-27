049312a9aa06aca39c968a1d9d71d1dd81052bae
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-27 00:29:02 -0500

    Set default to AzureADMyOrg with comments

--- cloud_shell/01-app_create.sh
+++ cloud_shell/01-app_create.sh
@@ -6,8 +6,9 @@ domain_name=`az rest --method get --url https://graph.microsoft.com/v1.0/domains
 app_name="sp_terraform_"${domain_name}"_"${RANDOM}
 echo "app_name="${app_name} >> ./environment
 
-# Create App Registration 
+# Create App Registration
+# Two options for sign-in-audience AzureADMyOrg, AzureADMultipleOrgs
 object_id=`az ad app create --display-name ${app_name} \
 --web-redirect-uris https://${domain_name} \
---sign-in-audience AzureADMultipleOrgs | jq -r '.id'`
+--sign-in-audience AzureADMyOrg | jq -r '.id'`
 echo "object_id="${object_id} >> ./environment
